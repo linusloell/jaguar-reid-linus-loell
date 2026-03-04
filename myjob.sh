ea7e5ca53feb2ae0b10b30947a05418fa0a55222
linusloell <58226209+linusloell@users.noreply.github.com>
2026-03-04 17:50:52 +0100

    setup using uv

---
+++ myjob.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eux
+#SBATCH --job-name=jaguar-reid
+#SBATCH --account=sci-demelo-computer-vision
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=linus.loell@student.hpi.de
+#SBATCH --partition=gpu-batch # -p
+#SBATCH --cpus-per-task=3 # -c
+#SBATCH --mem=32gb
+#SBATCH --gpus=1
+#SBATCH --time=1:00:00
+#SBATCH --output=jaguar-reid_%j.log # %j is job id
+
+
+echo '=================== GPU ================'
+source .venv/bin/activate
+
+echo '=================== GPU ================'
+nvidia-smi
+
+echo '=================== Notebook  ================'
+jupyter nbconvert --to notebook --execute jaguarreidentification-megadescriptor-arcfaceloss.ipynb
